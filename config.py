43a4796b8f7aa6f3b043c0d80ce3872b079e083f
Solcelle <iver.mortensen@gmail.com>
2022-04-07 02:16:24 +0200

    added fuel cans and top bar
    added fuel cans and top bar showing fuel amount and number of lives

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.font.init()
 
 # Display
 SCREEN_X = 1080
@@ -11,30 +12,53 @@ clock = pygame.time.Clock()
 
 BACKGROUND_COLOR = (20, 22, 30)
 
-TILE_SIZE = 40
-
 # Block
 BLOCK_COLOR = (120, 120, 160)
+TILE_SIZE = 40
 
-# Player
+
+# Player 2
 P1_CONTROLS = 1	# W, A, D, SPACE
+P1_COLOR = (200, 150, 0)
+P1_SPAWN = pygame.Vector2(140, 200)
+P1_TOP_BAR = pygame.Vector2(43, 10)
+
+# Player 2
 P2_CONTROLS = 2	# UP, LEFT, RIGHT, L-CTRL
+P2_COLOR = (100, 0, 100)
+P2_SPAWN = pygame.Vector2(940, 520)
+P2_TOP_BAR = pygame.Vector2(764, 10)
+
+# Player general
+PLAYER_HEIGHT = 30
+PLAYER_WIDTH = 20
 PLAYER_SPEED = 300
 BREAK_SPEED = 0.98
-ACCEL_SPEED = 0.006
+ACCEL_SPEED = 0.008
 CRASH_SPEED = 0.5
-ROTATION_SPEED = 400
+ROTATION_SPEED = 200
+
+# Fuel
+FUEL_GAUGE_HEIGHT = 20
+FUEL_GAUGE_WIDTH = 157
+FUEL_DELAY = 10
 
 # Bullet
 BULLET_HEIGHT = 4
 BULLET_WIDTH = 4
-BULLET_SPEED = 400
+BULLET_SPEED = 500
 BULLET_COLOR = (150, 150, 150)
 BULLET_DELAY = 1
 
-
 GRAVITY = 0.014
+
+# Image imports
 explosion_img = pygame.image.load("explosion.png").convert_alpha()
 explosion_img = pygame.transform.scale(explosion_img, (40, 40))
-spaceship_img = pygame.image.load("spaceship.png").convert_alpha()
-spaceship_img = pygame.transform.scale(spaceship_img, (40, 40))
+fuel_can_img = pygame.image.load("fuel_can.png").convert_alpha()
+fuel_can_img = pygame.transform.scale(fuel_can_img, (40, 40))
+# spaceship_img = pygame.image.load("spaceship.png").convert_alpha()
+# spaceship_img = pygame.transform.scale(spaceship_img, (40, 40))
+
+font = pygame.font.SysFont("Impact", 30)
+lives_txt = font.render('Lives:', True, (40, 40, 40))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import config
 import map
 import pygame
+pygame.font.init()
 
 '''Object'''
 class Object():
@@ -25,6 +26,10 @@ class Moving_object(Object):
 class Draw_object():
 	def draw_rect(self):
 		self.rect = pygame.draw.rect(config.screen, self.color, (self.vector_pos.x, self.vector_pos.y, self.width, self.height))
+	def draw_fuel_amount(self):
+		self.rect = pygame.draw.rect(config.screen, self.color, (self.vector_pos.x, self.vector_pos.y, self.fuel_amount, self.height))
+	def draw_rect_border(self):
+		self.rect = pygame.draw.rect(config.screen, self.border_color, (self.vector_pos.x - self.border, self.vector_pos.y - self.border, self.width + (self.border * 2), self.height + (self.border * 2)), self.border)
 	def draw_poligon(self):
 		self.rect = pygame.draw.polygon(config.screen, self.color, [p + self.vector_pos for p in self.points])
 	def draw_circle(self):
@@ -68,29 +73,81 @@ class Bullet(Moving_object, Draw_object):
 	def update(self):
 		self.move()
 		self.draw_circle()
-		
+
+'''Fuel gauge'''
+class Fuel_gauge(Object, Draw_object):
+	height = config.FUEL_GAUGE_HEIGHT
+	width = config.FUEL_GAUGE_WIDTH
+	def __init__(self, x, y, color):
+		super().__init__(x, y, Fuel_gauge.height, Fuel_gauge.width, color)
+		self.fuel_amount = Fuel_gauge.width
+		self.use_fuel = False
+		self.border = 3
+		self.border_color = (60, 60, 60)
+		self.fuel_consumption = 10
+	
+	def update(self):
+		self.draw_rect_border()		# Draws border around fuel gauge
+		self.draw_fuel_amount()		# Draws fuel amount in fuel gauge
+		self.update_fuel_amount()	# Updates fuel amount
+	
+	def update_fuel_amount(self):
+		if self.use_fuel:
+			self.fuel_amount -= self.fuel_consumption * time_passed
+
+'''Fuel can'''
+class Fuel_can(Object, Draw_object):
+	def __init__(self, x, y):
+		super().__init__(x, y, 40, 40, 0)
+		self.draw_rect()
+		self.collected = False
+		self.time_collected = 0
+		self.fuel_ready = False
+
+	def update(self):
+		if not self.collected:
+			self.draw_img(config.fuel_can_img, self.vector_pos.x, self.vector_pos.y)
+		else:
+			self.timer()
+	
+	
+	def timer(self):
+		# Check if next fuel can ready
+		if time.time() > (self.time_collected + config.FUEL_DELAY):
+			self.fuel_ready = True
+		# Spawn fuel if next fuel ready
+		if self.fuel_ready:
+			self.collected = False
+			self.time_collected = time.time()
+			self.fuel_ready = False
+
+
 '''Player'''
 class Player(Moving_object, Draw_object):
-	def __init__(self, x, y, height, width, speed, color):
-		super().__init__(x, y, height, width, speed, color)
+	def __init__(self, x, y, color, bar_pos):
+		super().__init__(x, y, config.PLAYER_HEIGHT, config.PLAYER_WIDTH, config.PLAYER_SPEED, color)
 		self.points = (pygame.Vector2(0, self.height * -0.75), pygame.Vector2(self.width / 2, self.height * 0.25), pygame.Vector2(self.width / -2, self.height * 0.25))
 		self.spawn = pygame.Vector2(x, y)
 		self.start = False
 		self.keys = [False, False, False, False, False]
-		self.lives = 3
+		self.lives = 6
 		self.respawn = False
 		self.explode = (False, 0, 0)
 		self.bullet_ready = True
 		self.bullet_time = 0
 		self.bullets = []
+		self.bar_pos = bar_pos
+		self.fuel = Fuel_gauge(bar_pos.x, bar_pos.y, self.color)
 
 	def update(self):
 		self.player_inputs()									# Executes player inputs
-		if self.start: self.move()								# Move object
 		self.explosion()										# Draw explosion
-		self.player_respawn()									# Respawn object
-		self.draw_poligon()										# Draw object
+		self.player_respawn()									# Respawn player
+		self.draw_poligon()										# Draw player
+		self.top_bar()											# Updates top bar with player info
+		if self.start: self.move()								# Move player
 		for block in blocks: self.check_collision(block)		# Check collisions with blocks
+		for item in items: self.check_collect(item)
 		
 		# Check if bullet hits
 		for player in players:
@@ -102,15 +159,17 @@ class Player(Moving_object, Draw_object):
 	# Executes players inputs
 	def player_inputs(self):
 		# If thrust...
-		if self.keys[0]:	
+		if self.keys[0] and self.fuel.fuel_amount > 0:	
 			thrust_dir = self.points[0].normalize()
 			self.vector_vel.x += thrust_dir.x * config.ACCEL_SPEED
 			self.vector_vel.y += thrust_dir.y * (config.ACCEL_SPEED * 4)
 			self.start = True
+			self.fuel.use_fuel = True
 		# Else reduce direction speed and add gravity
 		else:
 			self.vector_vel *= config.BREAK_SPEED
 			self.gravity()
+			self.fuel.use_fuel = False
 		# If rotate left...
 		if self.keys[1]:
 			self.points = [pygame.Vector2(p).rotate(-config.ROTATION_SPEED * time_passed) for p in self.points]
@@ -137,16 +196,33 @@ class Player(Moving_object, Draw_object):
 		if self.respawn:
 			self.vector_pos = pygame.Vector2(self.spawn.x, self.spawn.y)
 			self.vector_vel = pygame.Vector2(0, 0)
+			self.fuel.fuel_amount = self.fuel.width
 			self.respawn = False
 
-	# Check for collision with rect
+	# Updates top bar with player info
+	def top_bar(self):
+		# Update fuel gauge
+		self.fuel.update()
+
+		# Draw lives
+		self.draw_img(config.lives_txt, self.bar_pos.x + 180, self.bar_pos.y - 10)						# Draws lives text
+		num_lives_txt = pygame.font.SysFont("Impact", 30).render(str(self.lives), True, (40, 40, 40))	# Converts numb lives to text
+		self.draw_img(num_lives_txt, self.bar_pos.x + 260, self.bar_pos.y - 10)							# Draws numb lives text
+
+	# Check for collision with other object
 	def check_collision(self, other):
 		if pygame.Rect.colliderect(self.rect, other.rect):
+			# Sets explotion at position
 			self.explode = (True, self.vector_pos.x, self.vector_pos.y)
-			self.respawn = True
-			self.lives -= 1
-			self.start = False
-	
+			self.respawn = True		# Makes player respawn
+			self.lives -= 1			# Reduces lives
+			self.start = False		# Makes player not move after respawn
+
+	def check_collect(self, item):
+		if pygame.Rect.colliderect(self.rect, item.rect) and not item.collected:
+			self.fuel.fuel_amount = self.fuel.width
+			item.collected = True
+
 	# Check for explosion and draw explostion
 	def explosion(self):
 		if self.explode[0]:
@@ -209,8 +285,9 @@ def check_events():
 			if event.key == pygame.K_RIGHT:
 				players[1].keys[3] = False
 
-players = [Player(100, 100, 30, 20, config.PLAYER_SPEED, (100, 0, 100)), Player(100, 200, 30, 20, config.PLAYER_SPEED, (100, 0, 100))]
+players = [Player(config.P1_SPAWN.x, config.P1_SPAWN.y, config.P1_COLOR, config.P1_TOP_BAR), Player(config.P2_SPAWN.x, config.P2_SPAWN.y, config.P2_COLOR, config.P2_TOP_BAR)]
 blocks = []
+items = [Fuel_can(525, 90)]
 map1 = Map()
 map1.create()
 
@@ -230,6 +307,9 @@ while True:
 
 	# Update players
 	for player in players: player.update()
+
+	# Update items
+	for item in items: item.update()
 
 	# Update display too show new frame
 	pygame.display.update()
